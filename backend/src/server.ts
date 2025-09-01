bd9f03815d874a704c9472bd1fb52a9deb15ac8b
meganide <renashassan@hotmail.com>
2025-09-01 20:27:23 +0200

    chore: init projec

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 3000;
+const port = 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript + Node.js + Express!!ss');
