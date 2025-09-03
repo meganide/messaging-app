74406578e2d6f25a4ab06ce97ed4d41e33360d39
meganide <renashassan@hotmail.com>
2025-09-03 18:59:30 +0200

    refactor: remove unsued fucntion

--- backend/src/lib/jwt.ts
+++ backend/src/lib/jwt.ts
@@ -21,14 +21,6 @@ export function verifyToken(token: string): JwtPayload {
   return jwt.verify(token, JWT_SECRET) as JwtPayload;
 }
 
-export function decodeToken(token: string): JwtPayload | null {
-  try {
-    return jwt.decode(token) as JwtPayload;
-  } catch {
-    return null;
-  }
-}
-
 export const JWT_COOKIE_NAME = "auth-token";
 export const COOKIE_OPTIONS = {
   httpOnly: true,
